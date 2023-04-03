39b14fa4abc90d5f16819b8e4110f0db1585af35
cgit6 <109117801+cgit6@users.noreply.github.com>
2023-04-03 17:50:21 +0800

    2023/4/3

--- Algorithm Design/2.14_fs18.js
+++ Algorithm Design/2.14_fs18.js
@@ -1,9 +1,14 @@
 function fs(n) {
+  // 0
   if (n == 0) {
     return 0;
-  } else if (n == 1) {
+  }
+  // 1
+  else if (n == 1) {
     return 1;
-  } else {
+  }
+  // n>1
+  else {
     return fs(n - 1) + fs(n - 2);
   }
 }
--- Algorithm Design/2.15_array_of_arrays19.js
+++ Algorithm Design/2.15_array_of_arrays19.js
@@ -1,13 +1,15 @@
+
 let arrs = [[[["a", [["b", ["c"]], ["d"]]], [["e"]], [[["f", "g", "h"]]]]]];
 
 function collector(arr1) {
+  // 元素集合
   let result = [];
   helper(arr1);
   return result;
-
   function helper(arr2) {
     for (let i = 0; i < arr2.length; i++) {
       if (Array.isArray(arr2[i])) {
+        // 遞迴函式
         helper(arr2[i]);
       } else {
         result.push(arr2[i]);
